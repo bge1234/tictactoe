7a7b18b7e6ee1a4b3a1bec012baa8b0664440dad
Ben Ebel <bge1234@gmail.com>
2015-12-10 13:42:17 -0700

    limited to one space per square

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,29 @@
-// $("#square0").click(function() {
-//   $("#square0").append("<img class='xo' src='images/x.png' height='120' width='120'>");
-// });
+var turns = 0;
+var usedSquares = [];
 
 $("td").click(function() {
   var squareID = this.id;
-  $("#" + squareID).append("<img class='xo' src='images/x.png' height='120' width='120'>");
+  var squareTaken = false;
+
+  for(var i = 0; i< usedSquares.length; i++) {
+    if(usedSquares[i] === squareID) {
+      squareTaken = true;
+    }
+  }
+
+  if(!squareTaken) {
+    turns++;
+    usedSquares.push(squareID);
+    if(turns % 2 === 1)
+      $("#" + squareID).append("<img class='xo' src='images/x.png' height='120' width='120'>");
+    else
+      $("#" + squareID).append("<img class='xo' src='images/o.png' height='120' width='120'>");
+
+    if(turns >= 9)
+      gameOver();
+    }
 });
+
+function gameOver() {
+  alert("Game over!");
+};
